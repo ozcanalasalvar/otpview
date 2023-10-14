5f05877bfc592feb9bda604b9f1b45d7bdf85103
ozcanalasalvar <ozcanalasalvarr@gmail.com>
2023-10-15 01:16:34 +0300

    v1.0.5-alpha03

--- otpview/build.gradle.kts
+++ otpview/build.gradle.kts
@@ -36,7 +36,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.3.2"
     }
 
 }
@@ -47,7 +47,7 @@ publishing {
         create<MavenPublication>("release") {
             groupId = "com.ozcanalasalvar"
             artifactId = "otpview"
-            version = "1.0.5-alpha02"
+            version = "1.0.5-alpha03"
 
             artifact("$buildDir/outputs/aar/otpview-release.aar")
         }
